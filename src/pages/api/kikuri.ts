79e776b9a82c4a34001778df30602e26a692942c
durpyneko <89787577+durpyneko@users.noreply.github.com>
2024-03-03 20:55:07 +0200

    Update kikuri.ts

--- src/pages/api/kikuri.ts
+++ src/pages/api/kikuri.ts
@@ -21,7 +21,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const directory = path.join("public", "kikuri");
+  const directory = path.join(process.cwd(), "public", "kikuri");
   const randomFile = getRandomFile(directory);
   const filePath = path.join("/kikuri/", randomFile);
   res.status(200).json({ image: filePath });
